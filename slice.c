2ff08bdeb091cada973f1e4dbbc0ea5b237b3dab
FredEckert <fredeckert@home>
2012-08-20 04:56:02 -0400

    fwe090624 developed slice.c
    Modified split.c to split a file and timestamp the pieces. This will be
    used to slice the incoming audio stream into 1 minute long segments.

--- slice.c
+++ slice.c
@@ -22,9 +22,12 @@
 
 #include <config.h>
 
-#include <stdio.h>
-#include <getopt.h>
 #include <sys/types.h>
+#include <getopt.h>
+#include <time.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #include "system.h"
 #include "error.h"
@@ -144,21 +147,39 @@ GB 1000*1000*1000, G 1024*1024*1024, and so on for T, P, E, Z, Y.\n\
 static void
 next_file_name (void)
 {
+
+  char buffer[60];
+  char timestamp[100];
+  struct timeval tv;
+  time_t curtime;
+
+  gettimeofday(&tv, NULL); 
+  curtime=tv.tv_sec;
+
+  strftime(buffer,60,"slice-%Y%m%d_%T.",localtime(&curtime));
+  sprintf(timestamp,"%s%ld.raw",buffer,tv.tv_usec);
+  fprintf(stdout,_("%s%ld.raw\n"),buffer,tv.tv_usec);
+
   /* Index in suffix_alphabet of each character in the suffix.  */
   static size_t *sufindex;
 
+/*
   if (! outfile)
     {
-      /* Allocate and initialize the first file name.  */
-
+       Allocate and initialize the first file name.  
+*/
       size_t outbase_length = strlen (outbase);
+      size_t suffix_length = strlen (timestamp);
       size_t outfile_length = outbase_length + suffix_length;
       if (outfile_length + 1 < outbase_length)
 	xalloc_die ();
       outfile = xmalloc (outfile_length + 1);
       outfile_mid = outfile + outbase_length;
       memcpy (outfile, outbase, outbase_length);
-      memset (outfile_mid, suffix_alphabet[0], suffix_length);
+/*      memset (outfile_mid, suffix_alphabet[0], suffix_length);
+*/ 
+      memcpy (outfile_mid, timestamp, suffix_length);
+
       outfile[outfile_length] = 0;
       sufindex = xcalloc (suffix_length, sizeof *sufindex);
 
@@ -175,23 +196,7 @@ next_file_name (void)
 	free (dir);
       }
 #endif
-    }
-  else
-    {
-      /* Increment the suffix in place, if possible.  */
 
-      size_t i = suffix_length;
-      while (i-- != 0)
-	{
-	  sufindex[i]++;
-	  outfile_mid[i] = suffix_alphabet[sufindex[i]];
-	  if (outfile_mid[i])
-	    return;
-	  sufindex[i] = 0;
-	  outfile_mid[i] = suffix_alphabet[sufindex[i]];
-	}
-      error (EXIT_FAILURE, 0, _("Output file suffixes exhausted"));
-    }
 }
 
 /* Write BYTES bytes at BP to an output file.
@@ -403,7 +408,7 @@ main (int argc, char **argv)
   /* Parse command line options.  */
 
   infile = "-";
-  outbase = "x";
+  outbase = "";
 
   while (1)
     {
