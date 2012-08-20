2a52210ffab39d38dd0c32eb064726aec6dff401
FredEckert <fredeckert@home>
2012-08-20 04:26:13 -0400

    fwe090618 Major update #1: 'Seconds Since Epoch'

--- pySquelchGrapher.py
+++ pySquelchGrapher.py
@@ -1,38 +1,114 @@
-import pylab
+#pySquelchGrapher.py
+print "loading libraries...",
+import pylab, datetime, numpy
+print "complete"
 
-def loadData():
-        #returns Xs
-        import time, datetime, pylab
-        f=open('good.txt')
-        raw=f.readlines()
+def loadData(fname="log.txt"):
+        print "loading data...",
+        # load signal/duration from log file
+        f=open(fname)
+        raw=f.read()
         f.close()
-        onTimes=[]
-        timeStart=None
-        lastOn=False
+        raw=raw.replace('\n',' ')
+        raw=raw.split(" ")
+        signals=[]
         for line in raw:
-                if len(line)<10: continue
-                line = line.strip('\n').split(" ")
-                t=line[0]+" "+line[1]
-                t=t.split('.')
-                thisDay=time.strptime(t[0], "%Y-%m-%d %H:%M:%S")
-                e=time.mktime(thisDay)+float("."+t[1])
-                if timeStart==None: timeStart=e
-                if line[-1]==1: stat=True
-                else: stat=False
-                if not lastOn and line[-1]=="1":
-                        lastOn=e
-                else:
-                        onTimes.append([(lastOn-timeStart)/60.0,\
-                                                        (e-timeStart)/60.0])
-                        lastOn=False
-        return onTimes
+                if len(line)<3: continue
+                line=line.split(',')
+                sec=datetime.datetime.fromtimestamp(int(line[0]))
+                dur=int(line[1])
+                signals.append([sec,dur])
+        print "complete"
+        return signals
 
-times = loadData()
-pylab.figure(figsize=(8,3))
-for t in times:
-        pylab.fill([t[0],t[0],t[1],t[1]],[0,1,1,0],'k',lw=0,alpha=.5)
-pylab.axis([None,None,-3,4])
-pylab.title("A little QSO")
-pylab.xlabel("Time (minutes)")
-pylab.show()
+def findDays(signals):
+        # determine which days are in the log file
+        print "finding days...",
+        days=[]
+        for signal in signals:
+                day = signal[0].date()
+                if not day in days:
+                        days.append(day)
+        print "complete"
+        return days
+
+def genMins(day):
+        # generate an array for every minute in a certain day
+        print "generating bins...",
+        mins=[]
+        startTime=datetime.datetime(day.year,day.month,day.day)
+        minute=datetime.timedelta(minutes=1)
+        for i in xrange(60*60):
+                mins.append(startTime+minute*i)
+        print "complete"
+        return mins
+
+def fillMins(mins,signals):
+        print "filling bins...",
+        vals=[0]*len(mins)
+        dayToDo=signals[0][0].date()
+        for signal in signals:
+                if not signal[0].date() == dayToDo: continue
+                sec=signal[0]
+                dur=signal[1]
+                prebuf = sec.second
+                minOfDay=sec.hour*60+sec.minute
+                if dur+prebuf<60: # simple case, no rollover seconds
+                        vals[minOfDay]=dur
+                else: # if duration exceeds the minute the signal started in
+                        vals[minOfDay]=60-prebuf
+                        dur=dur+prebuf
+                        while (dur>0): # add rollover seconds to subsequent minutes
+                                minOfDay+=1
+                                dur=dur-60
+                                if dur< =0: break
+                                if dur>=60: vals[minOfDay]=60
+                                else: vals[minOfDay]=dur
+        print "complete"
+        return vals
+
+def normalize(vals):
+        print "normalizing data...",
+        divBy=float(max(vals))
+        for i in xrange(len(vals)):
+                vals[i]=vals[i]/divBy
+        print "complete"
+        return vals
+
+def smoothListGaussian(list,degree=10):
+        print "smoothing...",
+        window=degree*2-1
+        weight=numpy.array([1.0]*window)
+        weightGauss=[]
+        for i in range(window):
+                i=i-degree+1
+                frac=i/float(window)
+                gauss=1/(numpy.exp((4*(frac))**2))
+                weightGauss.append(gauss)
+        weight=numpy.array(weightGauss)*weight
+        smoothed=[0.0]*(len(list)-window)
+        for i in range(len(smoothed)):
+          smoothed[i]=sum(numpy.array(list[i:i+window])*weight)/sum(weight)
+        while len(list)>len(smoothed)+int(window/2):
+                smoothed.insert(0,smoothed[0])
+        while len(list)>len(smoothed):
+                smoothed.append(smoothed[0])
+        print "complete"
+        return smoothed
+
+signals=loadData()
+days=findDays(signals)
+for day in days:
+        mins=genMins(day)
+        vals=normalize(fillMins(mins,signals))
+        fig=pylab.figure()
+        pylab.grid(alpha=.2)
+        pylab.plot(mins,vals,'k',alpha=.1)
+        pylab.plot(mins,smoothListGaussian(vals),'b',lw=1)
+        pylab.axis([day,day+datetime.timedelta(days=1),None,None])
+        fig.autofmt_xdate()
+        pylab.title("147.120 MHz Usage for "+str(day))
+        pylab.xlabel("time of day")
+        pylab.ylabel("fractional usage")
+        pylab.show()
 
--- pySquelchLogger.py
+++ pySquelchLogger.py
@@ -1,40 +1,69 @@
-import alsaaudio, time, audioop, datetime
+#pySquelchLogger.py
+import time, random, alsaaudio, audioop
 inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE,alsaaudio.PCM_NONBLOCK)
-inp.setchannels(1)
-inp.setrate(4000)
-inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
-inp.setperiodsize(1)
+inp.setchannels(2)
+inp.setrate(1000)
+inp.setformat(alsaaudio.PCM_FORMAT_S8)
+inp.setperiodsize(100)
+addToLog=""
+lastLogTime=0
 
-squelch = False
-lastLog = 0
-dataToLog = ""
+testLevel=False ### SET THIS TO 'True' TO TEST YOUR SOUNDCARD
 
-def logIt(nowSquelch):
-        global dataToLog, lastLog
-        timeNow = datetime.datetime.now()
-        epoch = time.mktime(timeNow.timetuple())
-        if nowSquelch==True: nowSquelch=1
-        else: nowSquelch=0
-        logLine="%s %d\n"%(timeNow, nowSquelch)
-        print timeNow, nowSquelch
-        dataToLog+=logLine
-        if epoch-lastLog>10:
-                #print "LOGGING..."
-                f=open('squelch.txt','a')
-                f.write(dataToLog)
+def getVolEach():
+        # this is a quick way to detect activity.
+        # modify this function to use alternate methods of detection.
+        while True:
+                l,data = inp.read() # poll the audio device
+                if l>0: break
+        vol = audioop.max(data,1) # get the maximum amplitude
+        if testLevel: print vol
+        if vol>10: return True ### SET THIS NUMBER TO SUIT YOUR NEEDS ###
+        return False
+
+def getVol():
+        # reliably detect activity by getting 3 consistant readings.
+        a,b,c=True,False,False
+        while True:
+                a=getVolEach()
+                b=getVolEach()
+                c=getVolEach()
+                if a==b==c:
+                        if testLevel: print "RESULT:",a
+                        break
+        if a==True: time.sleep(1)
+        return a
+
+def updateLog():
+        # open the log file, append the new data, and save it again.
+        global addToLog,lastLogTime
+        #print "UPDATING LOG"
+        if len(addToLog)>0:
+                f=open('log.txt','a')
+                f.write(addToLog)
                 f.close()
-                lastLog = epoch
-                dataToLog=""
+                addToLog=""
+        lastLogTime=time.mktime(time.localtime())
+
+def findSquelch():
+        # this will record a single transmission and store its data.
+        global addToLog
+        while True: # loop until we hear talking
+                time.sleep(.5)
+                if getVol()==True:
+                        start=time.mktime(time.localtime())
+                        print start,
+                        break
+        while True: # loop until talking stops
+                time.sleep(.1)
+                if getVol()==False:
+                        length=time.mktime(time.localtime())-start
+                        print length
+                        break
+        newLine="%d,%d "%(start,length)
+        addToLog+=newLine
+        if start-lastLogTime>30: updateLog() # update the log
 
 while True:
-        l,data = inp.read()
-        if l:
-                vol = audioop.max(data,2)
-                #print vol #USED FOR CALIBRATION
-                if vol>800: nowSquelch = True
-                else: nowSquelch = False
-                if not nowSquelch == squelch:
-                        logIt(nowSquelch)
-                        squelch = nowSquelch
-        time.sleep(.01)
+        findSquelch()
 
